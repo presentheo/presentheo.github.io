f6d4948cb6c8762c30766798773eb6a1cec6ad16
presentheo <dbcjdgk@gmail.com>
2019-02-22 15:21:26 +0900

    edit project

---
+++ projects/mise-oneclick/precache-manifest.0d0ac958196414d60004aaf7c070200e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c277769bb509fdefdf83",
+    "url": "/projects/static/js/runtime~main.c277769b.js"
+  },
+  {
+    "revision": "2c7143b5ba849c57e9ae",
+    "url": "/projects/static/js/main.2c7143b5.chunk.js"
+  },
+  {
+    "revision": "e13de35fbb79b69070d0",
+    "url": "/projects/static/js/1.e13de35f.chunk.js"
+  },
+  {
+    "revision": "117a1be0650d0631c85ecfbbf0c19f16",
+    "url": "/projects/index.html"
+  }
+];
--- projects/mise-oneclick/service-worker.js
+++ projects/mise-oneclick/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.634504101164a63c8628a8447e440bb5.js"
+  "/projects/precache-manifest.0d0ac958196414d60004aaf7c070200e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/projects/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
