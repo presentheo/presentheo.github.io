bec54094e25055ee31a53d9795919598b7fce42f
presentheo <dbcjdgk@gmail.com>
2019-02-22 15:14:45 +0900

    add a new project

---
+++ projects/mise-oneclick/precache-manifest.634504101164a63c8628a8447e440bb5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3d674ffbbef613c8c10",
+    "url": "/static/js/main.d3d674ff.chunk.js"
+  },
+  {
+    "revision": "1a2e61c0b454e2a6dbdb",
+    "url": "/static/js/1.1a2e61c0.chunk.js"
+  },
+  {
+    "revision": "4dcd28c5b46a4b63c2440795a63f5bab",
+    "url": "/index.html"
+  }
+];
